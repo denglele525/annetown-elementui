afda52ef3ad16cf87ab4b349233abd9b62ea80c9
denglele <930441318@qq.com>
2019-12-05 00:03:13 +0800

    新增权限组树节点

--- src/components/sys/basic/PermissMana.vue
+++ src/components/sys/basic/PermissMana.vue
@@ -1,15 +1,90 @@
 <template>
     <div>
-        权限组
+        <div class="permissManaTool">
+            <el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
+                <template slot="prepend">ROLE_</template>
+            </el-input>
+            <el-input size="small" placeholder="请输入角色中文名" v-model="role.nameZh"></el-input>
+            <el-button type="primary" size="small" icon="el-icon-plus">添加角色</el-button>
+        </div>
+        <div class="permissManaMain">
+            <el-collapse accordion @change="change">
+                <el-collapse-item :title="r.nameZh" :name="r.id" v-for="(r,index) in roles" :key="index">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>可访问的资源</span>
+                            <el-button style="float: right; padding: 3px 0;color: #ff69a0;" icon="el-icon-delete"
+                                       type="text">
+                            </el-button>
+                        </div>
+                        <div>
+                            <el-tree show-checkbox
+                                     :data="allMenus" :props="defaultProps"></el-tree>
+                        </div>
+                    </el-card>
+                </el-collapse-item>
+            </el-collapse>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "PermissMana"
+        name: "PermissMana",
+        data() {
+            return {
+                role: {
+                    name: '',
+                    nameZh: ''
+                },
+                allMenus: [],
+                roles: [],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                }
+
+            }
+        },
+        mounted() {
+            this.initRoles();
+        },
+        methods: {
+            change(name) {
+                if (name) {
+                    this.initAllMenus();
+                }
+            },
+            initAllMenus() {
+                this.getRequest("/system/basic/permiss/menus").then(resp => {
+                    this.allMenus = resp;
+                })
+            },
+            initRoles() {
+                this.getRequest("/system/basic/permiss/").then(resp => {
+                    if (resp) {
+                        this.roles = resp;
+                    }
+                })
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .permissManaTool {
+        display: flex;
+        justify-content: flex-start;
+    }
+
+    .permissManaTool .el-input {
+        width: 300px;
+        margin-right: 6px;
+    }
+
+    .permissManaMain {
+        margin-top: 10px;
+        width: 700px;
+    }
 
 </style>
--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -15,9 +15,10 @@
             <el-table
                     :data="positions"
                     border
-                    size="small"
-                    stripe
-                    style="width: 70%">
+                    @selection-change="handleSelectionChange">
+                size="small"
+                stripe
+                style="width: 70%">
                 <el-table-column
                         type="selection"
                         width="55">
@@ -34,8 +35,15 @@
                 </el-table-column>
                 <el-table-column
                         prop="createDate"
-                        width="150"
+                        width="180"
                         label="创建时间">
+                </el-table-column>
+                <el-table-column
+                        label="是否启用">
+                    <template slot-scope="scope">
+                        <el-tag type="success" v-if="scope.row.enabled">已启用</el-tag>
+                        <el-tag type="danger" v-else>未启用</el-tag>
+                    </template>
                 </el-table-column>
                 <el-table-column label="操作">
                     <template slot-scope="scope">
@@ -52,13 +60,26 @@
                 </el-table-column>
             </el-table>
         </div>
+        <el-button @click="deleteMany" type="danger" size="small" style="margin-top: 8px"
+                   :disabled="multipleSelection.length==0">批量删除
+        </el-button>
         <el-dialog
                 title="修改职位"
                 :visible.sync="dialogVisible"
                 width="30%">
             <div>
-                <el-tag>职位名称</el-tag>
-                <el-input class="updatePosInput" size="small" v-model="updatePos.name"></el-input>
+              <div>
+                  <el-tag>职位名称</el-tag>
+                  <el-input class="updatePosInput" size="small" v-model="updatePos.name"></el-input>
+              </div>
+                <div>
+                    <el-tag>是否启用</el-tag>
+                    <el-switch
+                            v-model="updatePos.enabled"
+                            active-text="启用"
+                            inactive-text="禁用">
+                    </el-switch>
+                </div>
             </div>
             <span slot="footer" class="dialog-footer">
     <el-button size="small" @click="dialogVisible = false">取 消</el-button>
@@ -78,9 +99,11 @@
                 },
                 dialogVisible: false,
                 updatePos: {
-                    name: ''
+                    name: '',
+                    enabled:''
                 },
-                positions: []
+                positions: [],
+                multipleSelection: []
             }
         },
         mounted() {
@@ -138,6 +161,33 @@
                         this.positions = resp;
                     }
                 })
+            },
+            handleSelectionChange(val) {
+                this.multipleSelection = val;
+                console.log(val)
+            },
+            deleteMany() {
+                this.$confirm('此操作将永久删除【' + this.multipleSelection.length + '】几条记录, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                        let ids = '?';
+                        this.multipleSelection.forEach(item => {
+                            ids += 'ids=' + item.id + '&';
+                        })
+                        this.deleteRequest("/system/basic/pos/" + ids).then(resp => {
+                            if (resp) {
+                                this.initPositions();
+                            }
+                        })
+                    }
+                ).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
             }
         }
     }
